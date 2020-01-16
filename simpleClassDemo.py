757de446d4e8ba23914d3a730b2fa62fe15deeb8
Supriya Jadhav <sjadhav@SJADHAVM011048.local>
2020-01-16 15:15:33 -0600

    OOP : Simple Class defined

---
+++ simpleClassDemo.py
@@ -0,0 +1,13 @@
+class Stack:    # defining the Stack class
+    def __init__(self):    # defining the constructor function
+        print("Hi!")
+
+stackObject = Stack()    # instantiating the object
+
+
+'''
+1. the constructor's name is always __init__;
+2. it has to have at least one parameter (we'll discuss this later); the parameter is used to represent the newly created object - you can use the parameter to manipulate the object, and to enrich it with the needed properties; you'll make use of this soon;
+
+note: the obligatory parameter is usually named self - it's only a convention, but you should follow it - it simplifies the process of reading and understanding your code.
+'''
