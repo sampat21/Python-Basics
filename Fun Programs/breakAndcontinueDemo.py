2a102221e016b91626288fd55689a622e8c1eef6
Supriya Jadhav <sjadhav@SJADHAVM011048.local>
2020-01-16 16:56:05 -0600

    Fun Programs: simple if else program to demonstrate break and continue.

--- Fun Programs/breakAndcontinueDemo.py
+++ Fun Programs/breakAndcontinueDemo.py
@@ -1,3 +1,28 @@
+'''
+Scenario:
+Demonstrate simple program explaining use of break and continue keywords.
+
+Author: Supriya Jadhav
+
+Version: 1.0
+
+Date Created: Jan 16, 20120
+
+Test Data:
+The break instruction:
+Inside the loop. 1
+Inside the loop. 2
+Outside the loop.
+
+The continue instruction:
+Inside the loop. 1
+Inside the loop. 2
+Inside the loop. 4
+Inside the loop. 5
+Outside the loop.
+
+'''
+
 # break - example
 
 print("The break instruction:")
