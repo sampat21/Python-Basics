ed37f199009ee7509f10ad220009da1beea51cf8
Supriya Jadhav <sjadhav@SJADHAVM011048.local>
2020-01-16 14:03:20 -0600

    Exception example 1

---
+++ tryexceptExample.py
@@ -0,0 +1,9 @@
+firstNumber = int(input("Enter the first number: "))
+secondNumber = int(input("Enter the second number: "))
+
+try:
+    print(firstNumber / secondNumber)
+except:
+    print("This operation cannot be done.")
+
+print("THE END.")
