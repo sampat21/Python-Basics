817ce486230c420d86dbc72d3103260c553a2bf5
Supriya Jadhav <sjadhav@SJADHAVM011048.local>
2020-01-16 14:40:19 -0600

    String method: Replace method demo

---
+++ replaceMethodDemo.py
@@ -0,0 +1,4 @@
+# Demonstrating the replace() method
+print("www.netacad.com".replace("netacad.com", "pythoninstitute.org"))
+print("This is it!".replace("is", "are"))
+print("Apple juice".replace("juice", ""))
