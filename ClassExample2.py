7eba244ce594574a62458214ccacfc1aaf6f5358
Supriya Jadhav <sjadhav@SJADHAVM011048.local>
2020-01-16 15:18:45 -0600

    OOP: Declare a variable in constructor

---
+++ ClassExample2.py
@@ -0,0 +1,10 @@
+class Stack:
+    def __init__(self):
+        self.stackList = [1]
+
+stackObject = Stack()
+print(len(stackObject.stackList))
+
+'''
+Declaring a variable "stackList" inside the constructor of the Class stack
+'''
